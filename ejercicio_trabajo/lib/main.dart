5e37941d4cea172f026a4d78c7de1c9948424416
Angel Velez <aivelez1@espe.edu.ec>
2024-09-06 15:55:55 -0500

    abstraccion de datos

--- ejercicio_trabajo/lib/main.dart
+++ ejercicio_trabajo/lib/main.dart
@@ -1,18 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Product {
+  final int id;
+  final String title;
+  final String body;
+
+  Product({required this.id, required this.title, required this.body});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
+class ProductController extends ChangeNotifier {
+  List<Product> products = [];
+  bool isLoading = false;
+  int page = 1;
+
+  Future<void> fetchProducts() async {
+    isLoading = true;
+    notifyListeners();
+
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts?_page=$page&_limit=10'));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as List<dynamic>;
+      products.addAll(data.map((product) => Product.fromJson(product)).toList());
+      page++;
+    }
+    isLoading = false;
+    notifyListeners();
+  }
+}
+
+class ProductList extends StatefulWidget {
+  final List<Product> products;
+  final Function() onLoadMore;
+
+  const ProductList({
+    required this.products,
+    required this.onLoadMore,
+  });
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+class _ProductListState extends State<ProductList> {
+  final ScrollController _scrollController 
+ = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) 
+ {
+        widget.onLoadMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); 
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: widget.products.length + 1,
+      itemBuilder: 
+      (context, index) {
+        if (index < widget.products.length) {
+          return ListTile(
+            title: Text(widget.products[index].title),
+            subtitle: Text(widget.products[index].body),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ProductController productController = ProductController();
+
+  @override
+  void initState() {
+    super.initState();
+    productController.fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Lista de Productos'),
+        ),
+        body: FutureBuilder(
+          future: productController.fetchProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return ProductList(
+                products: productController.products,
+                onLoadMore: productController.fetchProducts,
+              );
+            }
+          },
         ),
       ),
     );
